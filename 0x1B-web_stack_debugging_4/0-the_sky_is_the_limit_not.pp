c0b99719d48c668272178b976e5cbad498d2d199
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-08-22 07:42:44 +0000

    correction

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # nginx server to accept more requests
 
-exec {'modify max file limit setting':
-	command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
-	path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+exec { 'modify max file limit setting':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
      }
